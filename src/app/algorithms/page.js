444ed782fdb0fe7d8c1d697343a8d4560c32241f
npjprogramming <149984996+npjprogramming@users.noreply.github.com>
2023-12-15 21:27:02 -0800

    Added what I could mostly page content and tried to fix some small things here and there.

--- src/app/algorithms/page.js
+++ src/app/algorithms/page.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import PageContent from '../components/PageContent';
 import PageTitle from '../components/PageTitle';
 import Footer from '../components/Footer';
+import { PAGES_MANIFEST } from 'next/dist/shared/lib/constants';
 
 export default function Home() {
 
@@ -12,21 +13,42 @@ export default function Home() {
 
       <Navbar />
 
-      <PageTitle title="algorithms"/>
+      <PageTitle title="Algorithms"/>
 
-      <div className="text-center">
+      <div className="flex flex-col items-center border border-solid inline-block border-black rounded-lg p-5 m-5">
 
-        <div className="font-bold p-5 m-5">
-          <PageContent content="Our Products:"/>
-       </div>
+        <div className="font-bold text-center underline text-lg p-5 m-5">
+          <PageContent content=" - Algorithmic Programming - "/>
+        </div>
+          
 
-        <div className="p-5 m-5">
-          <PageContent content=" - Product One"/>
+        <div className="text-center p-5 m-5">
+          <PageContent content="In programming, algorithms are generally a step-by-step set of instructions designed to accomplish a specific task or solve a particular problem. An algorithm can be thought of as a blueprint for the computer to follow and execute."/>
         </div>
 
-        <div className="p-5 m-5">
-          <PageContent content=" - Product Two"/>
-        </div>
+      </div>
+
+      <div className="flex flex-col text-center border border-solid inline-block border-black rounded-lg p-5 m-5">
+          
+          <div className="font-bold text-center underline text-lg p-5 m-5">
+            <PageContent content=" - Initial Evaluation - "/>
+          </div>
+
+          <div className="text-center p-5 m-5">
+            <PageContent content="When designing an algorithm in an attempt to streamline a process or application or complete a task it is always best to make sure you know exactly what has been asked to be created first. Evaluate what is being asked and attempt to formulate a way to make this work ideally before moving forward."/>
+          </div>
+
+      </div>
+
+      <div className="flex flex-col text-center border border-solid inline-block border-black rounded-lg p-5 m-5">
+          
+          <div className="font-bold text-center underline text-lg p-5 m-5">
+            <PageContent content=" - Charting - "/>
+          </div>
+
+          <div className="text-center p-5 m-5">
+            <PageContent content="Often drafting flow and sequence charts can be a big part of the process towards the design of a good algorithm. This is done both arguably for yourself and others working on the project to help understand what is being built and also how each part is supposed to function in the greater whole of the algorithm."/>
+          </div>
 
       </div>
 
--- src/app/linux/page.js
+++ src/app/linux/page.js
@@ -12,22 +12,29 @@ export default function Home() {
 
       <Navbar />
 
-      <PageTitle title="linux"/>
+      <PageTitle title="Linux"/>
 
-      <div className="text-center p-5 m-5">
+      <div className="flex flex-col items-center border border-solid inline-block border-black rounded-lg p-5 m-5">
 
-        <div className="font-bold underline p-5 m-5">
-          <PageContent content="Our Services:"/>
+        <div className="font-bold underline text-lg p-5 m-5">
+          <PageContent content=" - Linux - "/>
+        </div>
+        <div className="text-center p-5">
+          <PageContent content="The Linux operating system, created by Linus Torvalds in 1991, is a free and open-source operating system kernel. It is the core component of various Linux distributions used for servers, desktops, and embedded systems. Linux has gained popularity for its stability, security, and customization options."/>
         </div>
 
-        <div className="p-5 m-5">
-          <PageContent content="We can exponentially help to produce your companies' product."/>
-        </div>
-
-        <div className="p-5 m-5">
-          <PageContent content="We are the leading producers of Product and are industry leaders in our field."/>
-        </div>
+      </div>
       
+      <div className="flex flex-col items-center border border-solid inline-block border-black rounded-lg p-5 m-5">
+
+        <div className="font-bold underline text-lg p-5 m-5">
+          <PageContent content=" - Distributions - "/>
+        </div>
+
+          <div className="text-center p-5 m-5">
+            <PageContent content="Linux relies on user made distributions, often shortened to distros, that are developed by Linux users and often a distros development is geared towards a specific application or purpose. Linux distros are designed to meet a multitude of user needs, generally with an emphasis on specific features and uses for a distribution. Such as Ubuntu which is renowned for user-friendliness, and tends to be more popular among desktop users, while Debian prioritizes stability and serves as the foundation for many other distros. Other Linux distros like Red Hat Enterprise Linux (RHEL) targets enterprise environments and focus more on development support, or Arch Linux which appeals to users seeking customization and a minimalist development environment. In short, there are a wide variety of Linux distributions available entirely ."/>
+          </div>
+
       </div>
 
       <Footer />
